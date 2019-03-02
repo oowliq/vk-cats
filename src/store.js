e82d6fec414b295af726f53c33e5b00a2dffb2cb
Konstantin <ciless@Konstantins-iMac.local>
2019-03-02 16:24:08 +0300

    Init

--- src/store.js
+++ src/store.js
@@ -1,16 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import http from './helpers/http';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  state: {},
+  mutations: {},
+  actions: {},
+  getters: {},
 });
